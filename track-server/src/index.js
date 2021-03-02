e181806290a010a780e1c29bc9839bd150eed440
AnhTu <nguyenanhtu1999@gmail.com>
2021-03-02 16:57:42 +0300

    first commit

---
+++ track-server/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:admin@cluster0.hvxkt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instances');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('port 3000')
+});
