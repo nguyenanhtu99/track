146034665ee316d40f184fa8e35c48b7959db5f4
AnhTu <nguyenanhtu1999@gmail.com>
2021-03-15 21:20:09 +0300

    the auth context

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
         <Input
           label = 'Email'
+          value = {email}
+          onChangeText = {setEmail}
+          autoCapitalize="none"
+          autoCorrect={false}
         />
         <Input
+          secureTextEntry
           label = 'Password'
+          value = {password}
+          onChangeText = {setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
         />
       <Spacer>
         <Button
@@ -21,10 +33,22 @@ const SignupScreen = ({ navigation }) => {
           onPress={() => navigation.navigate('Signin')}
         />
       </Spacer>
-    </>
+    </View>
   )
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 100
+  }
+});
 
 export default SignupScreen;
